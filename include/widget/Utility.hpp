880cead33d98abe7a3bb9a5e57a2526175e834b1
Benno Straub <benno.straub@outlook.de>
2017-11-02 20:00:38 +0100

    Removed Padding until it's used again

--- include/widget/Utility.hpp
+++ include/widget/Utility.hpp
@@ -7,44 +7,6 @@ namespace widget {
 
 enum Unit { DP, PX, MM, CM };
 
-struct Padding {
-	float left, up, right, down;
-	Unit  uleft, uup, uright, udown;
-
-	constexpr inline
-	Padding(
-		float left, Unit uleft,
-		float up, Unit uup,
-		float right, Unit uright,
-		float down, Unit udown) :
-		left(left), up(up), right(right), down(down),
-		uleft(uleft), uup(uup), uright(uright), udown(udown)
-	{}
-
-	constexpr inline
-	Padding(float left, float up, float right, float down, Unit u = PX) :
-		Padding(left, u, up, u, right, u, down, u)
-	{}
-
-	constexpr inline
-	Padding(float x, Unit ux, float y, Unit uy) :
-		Padding(x, ux, y, uy, x, ux, y, uy)
-	{}
-
-	constexpr inline
-	Padding(float x, float y, Unit u = PX) :
-		Padding(x, u, y, u, x, u, y, u)
-	{}
-
-	constexpr inline
-	Padding(float f = 0, Unit u = PX) :
-		Padding(f, u, f, u)
-	{}
-
-	float width() const { return left + right; }
-	float height() const { return up + down; }
-};
-
 struct Area {
 	float x, y, width, height;
 	Unit  ux, uy, uwidth, uheight;
@@ -95,7 +57,6 @@ struct Area {
 };
 
 struct LayoutInfo {
-	Padding padding;
 	float minx = 0, maxx = std::numeric_limits<float>::infinity(), prefx = 20;
 	float miny = 0, maxy = std::numeric_limits<float>::infinity(), prefy = 20;
 	float weight = 1;
