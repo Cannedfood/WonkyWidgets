68d9660c9709b94fbd8bbeb3bd78eb4bb0ffb6cd
Benno Straub <benno.straub@outlook.de>
2018-01-20 20:25:15 +0100

    Added exponent to sliders

--- include/widget/widget/Slider.hpp
+++ include/widget/widget/Slider.hpp
@@ -11,9 +11,11 @@ class Slider : public Widget {
 	float mValue;
 	float mScale;
 	float mOffset;
+	float mExponent;
 
 	float handleSize() const noexcept;
 	float positionToValue(float x) const noexcept;
+	float valueToPosition(float x) const noexcept;
 protected:
 	void onDrawBackground(Canvas& canvas) override;
 	void on(Scroll  const& scroll) override;
@@ -26,14 +28,17 @@ public:
 
 	std::function<void(Slider*,float)> valueCallback;
 
-	inline float scale()       const noexcept { return mScale; }
-	inline float offset()      const noexcept { return mOffset; }
-	inline float value()       const noexcept { return mValue; }
+	inline float scale()    const noexcept { return mScale; }
+	inline float offset()   const noexcept { return mOffset; }
+	inline float value()    const noexcept { return mValue; }
+	inline float exponent() const noexcept { return mExponent; }
 
 	Slider* value (float f);
 	Slider* scale (float f);
+	Slider* exponent(float f);
 	Slider* offset(float f);
 	Slider* range (float min, float max);
+	Slider* range (float min, float max, float exponent);
 };
 
 } // namespace widget
--- src/widget/Slider.cpp
+++ src/widget/Slider.cpp
@@ -2,12 +2,15 @@
 
 #include "../../include/widget/Canvas.hpp"
 
+#include <cmath>
+
 namespace widget {
 
 Slider::Slider() :
 	mValue(.5f),
 	mScale(1),
-	mOffset(0)
+	mOffset(0),
+	mExponent(1)
 {
 	align(AlignFill);
 }
@@ -42,7 +45,14 @@ float Slider::handleSize() const noexcept { return width() * .2f; }
 float Slider::positionToValue(float x) const noexcept {
 	float hs = handleSize();
 	float w = width() - hs;
-	return offset() + scale() * ((x - hs * .5f) / w);
+	return offset() + scale() * powf((x - hs * .5f) / w, mExponent);
+}
+float Slider::valueToPosition(float value) const noexcept {
+	float f  = std::clamp((value - offset()) / scale(), 0.f, 1.f);
+	f = powf(f, 1 / mExponent);
+	float hs = handleSize();
+	float w  = width() - hs;
+	return f * w;
 }
 
 Slider* Slider::value(float f) {
@@ -65,18 +75,25 @@ Slider* Slider::value(float f) {
 	return this;
 }
 Slider* Slider::scale(float f)  { mScale = f; return this; }
+Slider* Slider::exponent(float f) {
+	mExponent = f;
+	return this;
+}
 Slider* Slider::offset(float f) { mOffset = f; return this; }
 Slider* Slider::range(float min, float max) {
 	offset(min);
 	scale(max - min);
 	return this;
 }
+Slider* Slider::range(float min, float max, float exp) {
+	range(min, max);
+	exponent(exp);
+	return this;
+}
 
 void Slider::onDrawBackground(Canvas& canvas) {
 	canvas.rect(100, {0, 0, width(), height()}, rgb(48, 48, 48));
-	float f = mValue / mScale;
-	float w = handleSize();
-	canvas.rect(100, {f * (width() - w), 0, width() * .2f, height()}, rgb(87, 87, 87));
+	canvas.rect(100, {valueToPosition(mValue), 0, handleSize(), height()}, rgb(87, 87, 87));
 }
 
 } // namespace widget
