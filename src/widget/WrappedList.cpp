ea4d2a509dc55c7b9c62e127b68c7acf4bbd3a3c
Benno Straub <benno.straub@outlook.de>
2018-05-05 19:18:47 +0200

    Improved wrapped list behavior - Heuristic now tries to get ~3 widgets next per line - Scrollable by default

--- src/widget/WrappedList.cpp
+++ src/widget/WrappedList.cpp
@@ -9,6 +9,7 @@ namespace wwidget {
 WrappedList::WrappedList() :
 	List()
 {
+	scrollable(true);
 	align(AlignFill);
 }
 WrappedList::WrappedList(Widget* addTo) :
@@ -22,7 +23,12 @@ PreferredSize WrappedList::onCalcPreferredSize() {
 	result.minx  = result.miny  = 0;
 	result.maxx  = result.maxy  = 0;
 	result.prefx = result.prefy = 0;
+
+	float width_sum = 0.f;
+	size_t child_count = 0;
 	eachChild([&](Widget* w) {
+		child_count++;
+
 		auto& size = w->preferredSize();
 		result.minx   = std::max(result.minx, size.minx + w->padX());
 		result.miny   = std::max(result.miny, size.miny + w->padY());
@@ -32,12 +38,20 @@ PreferredSize WrappedList::onCalcPreferredSize() {
 		if(flow() & BitFlowHorizontal) {
 			result.prefx += size.prefx + w->padX();
 			result.prefy  = std::max(result.prefy, size.prefy + w->padY());
+			width_sum += size.prefy + w->padY();
 		}
 		else {
 			result.prefy += size.prefy + w->padY();
 			result.prefx  = std::max(result.prefx, size.prefx + w->padX());
+			width_sum += size.prefx + w->padX();
 		}
 	});
+
+	if(flow() & BitFlowHorizontal)
+		result.prefy = std::max(result.prefy, width_sum / child_count * 3);
+	else
+		result.prefx = std::max(result.prefx, width_sum / child_count * 3);
+
 	result.sanitize();
 	return result;
 }
