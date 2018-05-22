7db4a8ae51bab4a4822103362c6f348016362bb7
Benno Straub <benno.straub@outlook.de>
2018-05-22 21:52:45 +0200

    Added ressources as a thing the Applet does

--- include/wwidget/Applet.hpp
+++ include/wwidget/Applet.hpp
@@ -6,12 +6,27 @@ namespace wwidget {
 
 class Font;
 
+enum RessourceId {
+	URL_ROOT,
+	URL_LIBRARIES,
+	URL_BINARIES,
+	URL_HOME,
+	URL_PICTURES,
+	URL_DOCUMENTS,
+	URL_MUSIC,
+	URL_CACHE_DIR,
+	URL_TMP_DIR,
+	URL_CONFIG_DIR,
+};
+
 class Applet {
 public:
 	Applet();
 	virtual ~Applet();
 
 	virtual void defer(std::function<void()>) = 0;
+
+	virtual std::string getRessource(RessourceId res);
 
 	virtual void loadImage(std::function<void(std::shared_ptr<Bitmap>)>, std::string const& url) = 0;
 	virtual void loadFont(std::function<void(std::shared_ptr<Font>)>, std::string const& url) = 0;
--- src/Applet.cpp
+++ src/Applet.cpp
@@ -5,4 +5,24 @@ namespace wwidget {
 Applet::Applet() {}
 Applet::~Applet() {}
 
+std::string Applet::getRessource(RessourceId res) {
+	// TODO: windows compatibility
+	switch(res) {
+		case URL_ROOT: return "/";
+		case URL_LIBRARIES: return "/usr/lib/";
+		case URL_BINARIES: return "/usr/bin/";
+		case URL_HOME: {
+			const char* result = getenv("HOME");
+			return result ? result : "";
+		}
+		case URL_PICTURES:   return getRessource(URL_HOME) + std::string("/Pictures/");
+		case URL_DOCUMENTS:  return getRessource(URL_HOME) + std::string("/Documents/");
+		case URL_MUSIC:      return getRessource(URL_HOME) + std::string("/Music/");
+		case URL_CACHE_DIR:  return getRessource(URL_TMP_DIR);
+		case URL_TMP_DIR:    return getRessource(URL_ROOT) + std::string("/tmp/");
+		case URL_CONFIG_DIR: return getRessource(URL_HOME) + std::string("/.config/");
+		default: return "";
+	}
+}
+
 } // namespace wwidget
