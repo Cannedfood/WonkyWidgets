4613fffdf53aab09a04975a3b2c93ba676fa88c3
Benno Straub <benno.straub@outlook.de>
2018-06-01 21:44:12 +0200

    Updated wwidget.hpp and moved it

---
+++ include/wwidget.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "wwidget/Widget.hpp"
+
+#include "wwidget/Window.hpp"
+
+#include "wwidget/widget/Button.hpp"
+#include "wwidget/widget/Dialogue.hpp"
+#include "wwidget/widget/Form.hpp"
+#include "wwidget/widget/Image.hpp"
+#include "wwidget/widget/Knob.hpp"
+#include "wwidget/widget/List.hpp"
+#include "wwidget/widget/ProgressBar.hpp"
+#include "wwidget/widget/Slider.hpp"
+#include "wwidget/widget/Text.hpp"
+#include "wwidget/widget/TextField.hpp"
+#include "wwidget/widget/WrappedList.hpp"
