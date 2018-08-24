fada45c2f5e80e287083c79d7c3ac9d0dfe99820
Benno Straub <benno.straub@outlook.de>
2018-08-24 15:47:59 +0200

    Fixed #define WWIDGET_EXTERNAL_STBI

--- src/thirdparty/Thirdparty.cpp
+++ src/thirdparty/Thirdparty.cpp
@@ -2,7 +2,7 @@
 	extern "C" {
 		#include "nanovg.inl"
 	}
-#elif !defined(WWIDGET_EXTERNAL_STBIMAGE)
+#elif !defined(WWIDGET_EXTERNAL_STBI)
 	#define STB_IMAGE_IMPLEMENTATION
 	#include "stb_image.h"
 #endif
