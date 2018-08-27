e89faf44ef2afc1d0eaa552eca89d7ec2a450028
Benno Straub <benno.straub@outlook.de>
2018-08-27 13:54:47 +0200

    Fixes to shared_ptr

--- include/wwidget/thirdparty/stx/shared_ptr.hpp
+++ include/wwidget/thirdparty/stx/shared_ptr.hpp
@@ -131,13 +131,13 @@ public:
 
 	shared(Tptr data) :
 		m_value(data),
-		m_block(new pointer_shared_block<T, std::default_delete<T>>(data))
+		m_block(!data ? nullptr : new pointer_shared_block<T, std::default_delete<T>>(data))
 	{}
 
 	template<class Deleter>
 	shared(Tptr data, Deleter del) :
 		m_value(data),
-		m_block(new pointer_shared_block<T, Deleter>(data, del))
+		m_block(!data ? nullptr : new pointer_shared_block<T, Deleter>(data, del))
 	{}
 
 	// Move
